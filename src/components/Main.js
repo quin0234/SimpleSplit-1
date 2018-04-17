725723a796f756f8be85e013f4d06f4858a57e13
quin0234 <quin0234@algonquinlive.com>
2018-04-17 18:29:24 -0400

    Push Notes code add to main.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,8 +1,44 @@
 import React, { Component } from 'React';
 import { Container, Text } from 'native-base';
 import FooterNav from './FooterNav';
+import Expo from 'expo';
+import firebase from 'firebase';
+
+async function register() {
+    // Remote notifications do not work in simulators, only on device
+  if (!Expo.Constants.isDevice) {
+    return;
+  }
+  let { status } = await Expo.Permissions.askAsync(
+    Expo.Permissions.NOTIFICATIONS,
+  );
+  if (status !== 'granted') {
+    return;
+  }
+    let value = await Expo.Notifications.getExpoPushTokenAsync();
+  console.log('Our token', value);
+  /// Send this to a server
+
+    let currentUser = firebase.auth().currentUser
+    firebase.database().ref(`/token/${currentUser.uid}/`).set({token: value});
+}
 
 class Main extends Component{
+    componentWillMount(){
+        register()
+        this.listener = Expo.Notifications.addListener(this.handleNotification);
+    }
+
+    componentWillUnmount() {
+        this.listener && this.listener.remove();
+      }
+    
+      handleNotification = ({ origin, data }) => {
+        console.log(
+          `Push notification ${origin} with data: ${JSON.stringify(data)}`,
+        );
+      };
+
     render(){
         return(
             <Container>
